41afeb21fc0839d4b9fa2e6822e6824052d9b746
Cynthia London <cynthe17@gmail.com>
2018-02-17 08:04:57 -0600

    add

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fully stacked burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Avacado Blue Cheese Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Swiss Cheese Mushroom Burger', FALSE);
